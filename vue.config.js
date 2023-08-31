155b74d35a9bf1bf4fc422aae3c7299739c1c10f
theIllEar <theillear@gmail.com>
2023-08-31 16:03:16 +0300

    lol

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: '/vue/',
-});
+}); 
 
 // import { defineConfig } from '@vue/cli-service';
 // export default defineConfig({

fcadeaa1195d9dd1e0715d1ddb24c9060f48e2fe
theIllEar <theillear@gmail.com>
2023-09-01 15:17:16 +0300

    Initial dist subtree commit

--- src/js/custom/errors.js
+++
@@ -1,9 +0,0 @@
-const ERROR_CODES = {
-  EMAIL_NOT_FOUND: 'Пользователь с таким Email не найден',
-  EMAIL_REPEAT: 'Пользователь с такой почтой уже существует',
-  INVALID_PASSWORD: 'Неверный пароль',
-};
-
-export const error = (code) => {
-  return ERROR_CODES[code] ? ERROR_CODES[code] : `Неизвестная ошибка: ${code}`;
-};
